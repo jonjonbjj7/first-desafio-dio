c125c998f5563e2ec6c3c410dbd97e1690c10577
jonjonbjj7 <jonjonlopes00@gmail.com>
2024-07-19 12:25:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,226 @@
+//saída
+console.log("Bem vindo ao mundo dos heróis")
+
+//saída
+console.log("Esse jogo só é permitido para +18")
+
+//saída
+console.log("Voçê é maior de 18 anos?")
+
+//variável
+let idadeMinima = "18"
+let idadeUsuario = "22"
+let idadePermitida = idadeUsuario >= idadeMinima
+
+//saída
+console.log(idadePermitida)
+
+//saída
+console.log("Qual é o seu usuário e senha?")
+
+// variável
+let usuario = "jonjon"
+    senha = "112233"
+
+//saída
+console.log("usuário:"+usuario)
+
+console.log("senha:"+senha);
+
+console.log(usuario+ " entrou no mundo")
+
+//saída
+console.log("quais são todos os heróis?" )
+
+//variável
+let nomeHerois = ["mamonas","jeff","cyclopedia","bariloche","mister m","lanterna azul","jean","diguidin"]
+const especificacoesDosHerois = [
+["nome: mamonas", "xp: 100", "nível: ferro"],
+
+["nome: jeff", "xp: 2000", "nível: bronze"],
+
+["nome: cyclopedia", "xp: 3000", "nível: prata"], 
+
+["nome: bariloche", "xp: 5500", "nível: ouro"], 
+
+["nome: mister m", "xp: 7500", "nível: platina"],
+
+["nome: lanterna azul", "xp: 8200", "nível: ascedente"],
+
+["nome: jean", "xp: 9500", "nível: imortal"],
+
+["nome: diguidin", "xp: 1200", "nível: radiante"],]
+
+console.log(nomeHerois)
+
+console.log(especificacoesDosHerois[0])
+
+console.log(especificacoesDosHerois[1])
+
+console.log(especificacoesDosHerois[2])
+
+console.log(especificacoesDosHerois[3])
+
+console.log(especificacoesDosHerois[4])
+
+console.log(especificacoesDosHerois[5])
+
+console.log(especificacoesDosHerois[6])
+
+console.log(especificacoesDosHerois[7])
+
+//saída
+console.log("Seu herói será de acordo com o nível de sua conta")
+
+//saída
+console.log("Voçê está no nível 1")
+
+//saída
+console.log("Desbloqueará os heróis conforme for aumentando seu nível no jogo")
+
+//saída
+console.log("Seu herói é o " + nomeHerois[0])
+
+//variável
+const enfrentarChefao = "Ganhe do chefão e aumente seu nível"
+const parabens = "Parabéns, voçê matou o chefão"
+const nivelAumentado = "Seu nível foi aumentado"
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 2; contador < 3; contador++){
+console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[1])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 3; contador < 4; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[2])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 4; contador < 5; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[3])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 5; contador < 6; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[4])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 6; contador < 7; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[5])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 7; contador < 8; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[6])
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 8; contador < 9; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[7])
+
+//saída
+console.log("Para se tornar o héroi mais forte da história precisa vencer mais dois chefões")
+
+//saída
+console.log(enfrentarChefao)
+console.log(parabens)
+console.log(nivelAumentado)
+
+//laços de repetição
+for (let contador = 9; contador < 10; contador++){
+    console.log("Seu nível agora é " + contador)}
+
+//saída
+console.log("Seu herói agora é o " + nomeHerois[7])
+
+//saída
+console.log("Voçê tem a opção de enfrentar o último chefão")
+console.log("caso enfrente e vença, o mundo se tornará melhor, caso perca, o mundo irá ficar pior")
+console.log("Voçê irá enfrentar o último chefão?")
+
+let descisão = 'sim'
+    descisão1 = 'Sim, vou enfrentar o chefão'
+switch (descisão){
+ case "não":
+console.log(descisão);
+ break;
+ case "sim":
+    console.log(descisão1)
+    break;
+}
+
+//saída
+console.log("parabéns, voçê venceu o último chefão, agora voçê chegou no ultimo nível que é o 10, seu herói é o mais forte")
+console.log("O Herói de nome " + nomeHerois[7] + " está no nível 10")
+console.log('parabéns, voçê zerou esse jogo')
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
